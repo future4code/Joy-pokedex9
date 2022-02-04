73b45900334612c0e67f57246c4022468a623e6b
Athos de Oliveira <64941377+athosoli@users.noreply.github.com>
2022-02-03 23:03:06 -0300

    implementando router e uma estilizada simples num botao.

---
+++ pokedexter/src/Components/Cardpokemon.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+import Axios from "axios";
+
+
+
+export const Cardpokemon = () => {
+    const history = useHistory();
+
+    history.push("/Detalhes");
+
+
+};
+
+
