72c2d28f8d490d1a2576d0bed9c5f0d7e1e47fbf
luizphs14 <marques.luiz@outlook.com>
2022-02-09 21:23:01 -0300

    detalhes

---
+++ pokedex/src/Components/layout/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import PokemonList from '../pokemon/PokemonList';
+import SearchBar from '../search/SearchBar';
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col">
+          <PokemonList />
+          <SearchBar />
+        </div>
+      </div>
+    );
+  }
+}
